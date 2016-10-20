996f014daf6f0b446951647a68bb7ac5404e02f4
Robbie Selwyn <robbie@selwynsoftware.com>
2016-10-19 19:36:33 -0700

    Added a script that optimizes code

---
+++ optimizer.py
@@ -0,0 +1,21 @@
+import sys
+import re
+
+f_in = open(sys.argv[1], "r")
+lines = f_in.readlines()
+
+redone_file = []
+for count, line in enumerate(lines):
+	redone_file.append(re.sub("\[\d\]", "COUNT", line))
+
+fixes = 0
+
+for i, v in enumerate(redone_file[:len(redone_file)-2]):
+	if v == redone_file[i+1] and v == redone_file[i+2]:
+		print "Please fix line #" + str(i+1) + "-" + str(i+3)
+		fixes += 1
+
+if fixes == 0:
+	print "Nice Job.  This script couldn't find any areas to reduce your memory allocation."
+else:
+	print "{} Errors were pointed out.".format(fixes)
